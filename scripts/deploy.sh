c8b067820f3189e641d8455adbed5b016f2e454b
Rodolphe Nivelet <rodolphe.nivelet@gmail.com>
2017-06-10 01:50:32 +0200

    Test travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+set -e
+
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+
+REPO=`git config remote.origin.url`
+
+echo "Starting to update gh-pages\n"
+
+cp -R dist $HOME/dist
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+git clone --quiet --branch=gh-pages $REPO gh-pages > /dev/null
+
+cd gh-pages
+
+cp -Rf $HOME/dist/* .
+
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages > /dev/null
+
+echo "Done updating gh-pages\n"
+
+else
+ echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+fi;
