472473e417a58b8da77a925cc73d90bf8b64b2c6
Rodolphe Nivelet <rodolphe.nivelet@gmail.com>
2017-06-10 02:47:25 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,15 @@ REPO=`git config remote.origin.url`
 
 echo "Starting to update gh-pages"
 
-cp -R dist .tmp
+cp -R dist $HOME/
+cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
-git clone --quiet --branch=gh-pages $REPO gh-pages > /dev/null
+git clone --quiet --branch=gh-pages "https://4c85bf70377c442858e7d48aa2a28533569d6679@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages > /dev/null
 
 cd gh-pages
 
-cp -Rf ../.tmp/* .
+cp -Rf $HOME/dist/* .
 
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
